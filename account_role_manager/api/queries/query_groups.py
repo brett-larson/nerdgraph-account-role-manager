456fb123e94f9f6bd55c572a3d9019689bd9a390
Brett Larson <larson.brett.s@gmail.com>
2025-03-06 18:34:43 -0500

    Create query_groups.py

---
+++ account_role_manager/api/queries/query_groups.py
@@ -0,0 +1,20 @@
+"""
+{
+  actor {
+    organization {
+      userManagement {
+        authenticationDomains(id: "YOUR_AUTHENTICATION_DOMAIN_ID") {
+          authenticationDomains {
+            groups {
+              groups {
+                displayName
+                id
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+"""
