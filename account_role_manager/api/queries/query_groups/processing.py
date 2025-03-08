aa97cb9184d3500f975d54f1f88b75650fb7a643
Brett Larson <larson.brett.s@gmail.com>
2025-03-08 13:03:09 -0500

    Update processing.py

--- account_role_manager/api/queries/query_groups/processing.py
+++ account_role_manager/api/queries/query_groups/processing.py
@@ -1,7 +1,7 @@
 from account_role_manager.utils import Logger
 
 # Create logger for the module
-logger = Logger(__name__).get_logger()  
+logger = Logger(__name__).get_logger()
 
 def extract_cursor(response_data, domain_index=0):
     try:
