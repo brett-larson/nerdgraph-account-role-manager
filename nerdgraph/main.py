459456e8d8402fe6062f6221fee1e07040aee1a6
Brett Larson <larson.brett.s@gmail.com>
2025-03-08 19:25:02 -0500

    Update main.py

--- nerdgraph/main.py
+++ nerdgraph/main.py
@@ -1,3 +1,17 @@
-import workflow
+from workflow import get_auth_domain_workflow
+from utils.logger import Logger
+from utils.nerdgraph_client import NerdGraphClient
+
+# Create logger for the module
+logger = Logger(__name__).get_logger()
 
 if __name__ == '__main__':
+
+    logger.info('********** Starting NerdGraph App **********.')
+
+    client = NerdGraphClient()
+
+    get_auth_domain_workflow(client)
+
+
+
